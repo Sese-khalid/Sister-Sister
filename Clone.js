ce11d1628985049d6d3c8acbcd672e818ed8d92b
Sese-khalid <Sese-khalid@users.noreply.github.com>
2018-04-25 13:16:22 -0400

    updates

---
+++ Clone.js
@@ -0,0 +1,39 @@
+﻿
+
+
+<script>
+var quotes= {
+First : "Here",
+Second : "is",
+Third : "jane" };
+document.getElementById("demo").innerHTML = quotes.First;
+document.getElementById("demo1").innerHTML = quotes.Second;
+document.getElementById("demo2").innerHTML = quotes.Third;
+
+var share=document.querySelector(".total");
+var shareClone = total.cloneNode(false);
+
+ document.querySelector(".here").appenChild
+   (shareClone);
+
+</script>
+
+
+var quotes= {
+First : "Like branches on a tree, our lives may grow in different directions 
+	 yet our roots remain as one.",
+Second : "Call it a clan, call it a network, call it a tribe, call it a family. <br>
+	 Whatever you call it, whoever you are, you need one. - Jane Howard",
+Third : "Time is mapped with laughter and cheers, <br>
+the long road sometimes landmarked with sadness and tears.<br>
+
+Elders pass and children grow,<br>
+has it been that long we ask, where does the time go?<br>
+
+Not every chance to gather is taken.<br>
+"We'll see them next time" we say and hope we're not mistaken.<br>
+
+A chance like now comes once in a lifetime it seems,<br>
+when the bright light of family is nurtured and beams,<br>
+there will be laughter and time to reminisce<br>
+we will all be proud this is a chance we did not miss.- Ryan Guerrero <br>" };
